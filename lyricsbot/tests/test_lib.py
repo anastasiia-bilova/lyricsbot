3b5cb65ad2d8f7ff04a66de9e2f041146e0a87b4
Anastasia Bilova <anastasiabilova@gmail.com>
2018-04-10 18:25:33 +0300

    Fix imports error

--- lyricsbot/tests/test_lib.py
+++ lyricsbot/tests/test_lib.py
@@ -5,7 +5,11 @@ import unittest
 
 from ddt import ddt, data, unpack
 
-from lyricsbot.lib.lib import join, split
+try:
+    from lib.lib import join, split
+# pylint:disable=bare-except
+except:  # noqa: E722 # Python 3.5 does not contain `ModuleNotFoundError`
+    from lyricsbot.lib.lib import join, split
 
 
 @ddt
