381c06ca50fff6951ac3b87d46cf64584fa11b80
Anastasia Bilova <anastasiabilova@gmail.com>
2018-04-10 18:24:52 +0300

    Delete environment variable local

--- lyricsbot/app.py
+++ lyricsbot/app.py
@@ -163,6 +163,3 @@ if __name__ == '__main__':
             host="0.0.0.0",
             port=int(os.environ.get('PORT', 5000))
         )
-
-    # if os.environ['ENVIRONMENT'] == 'local':
-    #     bot.polling()
