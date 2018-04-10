1f89d2143b08ef9b7ef445fc4e2399fa40f82c38
Anastasia Bilova <anastasiabilova@gmail.com>
2018-04-10 16:35:49 +0300

    Add doc-strings

--- lyricsbot/domains/genius/__init__.py
+++ lyricsbot/domains/genius/__init__.py
@@ -1,12 +1,3 @@
 """
 Init.
 """
-# from lyricsbot.domains.genius.genius import (
-#     format_request_data_url,
-#     parse_lyrics,
-#     get_song_text,
-# )
-# from lyricsbot.domains.genius.utils import (
-#     make_suitable_url_parameters,
-#     remove_punctuation_symbols,
-# )
