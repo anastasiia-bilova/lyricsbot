e4ecf1e452825e0688473eadbf3a6f59c4716b3e
Anastasia Bilova <anastasiabilova@gmail.com>
2018-04-11 13:17:15 +0300

    Delete bot token

--- lyricsbot/config.py
+++ lyricsbot/config.py
@@ -2,6 +2,6 @@
 Configs for Telegram bot.
 """
 
-# TOKEN = '393158140:AAFxXeQMxM8ELJ7Cs-3lqXHTuYqEYpDJIYU'
 TOKEN = '589737790:AAEc0Y3KYi4V7x5w98zAnRgNa634xX0dODM'
+
 URL = 'postgres://rikdrycw:U6jesojhjg6zzAjw0ijlDjqcvi6LTWEb@baasu.db.elephantsql.com:5432/rikdrycw'
