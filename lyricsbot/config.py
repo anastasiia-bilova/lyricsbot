237decb32b38bb192c863099a0c4a6b4b5d12b94
Anastasia Bilova <anastasiabilova@gmail.com>
2018-04-12 14:16:23 +0300

    Add token for testbot

--- lyricsbot/config.py
+++ lyricsbot/config.py
@@ -2,6 +2,6 @@
 Configs for Telegram bot.
 """
 
-# TOKEN = '589737790:AAEc0Y3KYi4V7x5w98zAnRgNa634xX0dODM'
+TOKEN = '478249668:AAHiatNG8BhmB_6FKdDKNyNshXrX1yoOIik'
 
 URL = 'postgres://rikdrycw:U6jesojhjg6zzAjw0ijlDjqcvi6LTWEb@baasu.db.elephantsql.com:5432/rikdrycw'
